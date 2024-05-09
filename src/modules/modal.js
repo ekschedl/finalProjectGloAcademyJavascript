a91b61711694ef5f97161e2840fbca51b971475f
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-05-09 22:12:59 +0200

    + modal for button-services

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -5,30 +5,34 @@ const modal = () => {
   const closeButton = document.querySelector(".modal-close");
   const modalOverlay = document.querySelector(".modal-overlay");
   const modalCallback = document.querySelector(".modal-callback");
+  const buttonServices = document.querySelector(".button-services");
+  const colSmFirstElement = document.querySelector(".col-sm-4.first");
 
-  callbackBtns.forEach((callbackBtn) => {
-    callbackBtn.addEventListener("click", () => {
-      modalCallback.style.display = "block";
-      modalOverlay.style.display = "block";
-      fadeIn(modalCallback);
-    });
-  });
+  const openModal = () => {
+    modalCallback.style.display = "block";
+    modalOverlay.style.display = "block";
+    fadeIn(modalCallback);
+  };
 
-  closeButton.addEventListener("click", () => {
+  const closeModal = () => {
     fadeOut(modalCallback);
     setTimeout(() => {
       modalOverlay.style.display = "none";
       modalCallback.style.display = "none";
     }, 300);
+  };
+
+  callbackBtns.forEach((callbackBtn) => {
+    callbackBtn.addEventListener("click", openModal);
   });
 
-  modalOverlay.addEventListener("click", () => {
-    fadeOut(modalCallback);
-    setTimeout(() => {
-      modalOverlay.style.display = "none";
-      modalCallback.style.display = "none";
-    }, 200);
-  });
+  buttonServices.addEventListener("click", openModal);
+
+  colSmFirstElement.addEventListener("click", openModal);
+
+  closeButton.addEventListener("click", closeModal);
+
+  modalOverlay.addEventListener("click", closeModal);
 };
 
 export default modal;
