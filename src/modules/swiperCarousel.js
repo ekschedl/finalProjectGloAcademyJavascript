df5ff600d2ecbfe7a2f895cb5ec9414396301feb
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-05-10 01:44:45 +0200

    correction

--- src/modules/swiperCarousel.js
+++ src/modules/swiperCarousel.js
@@ -1,43 +1,36 @@
 import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
 
-const initSwiperCarousel = () => {
-  const initializeSwiper = () => {
-    const swiperContainer = document.querySelector(".swiper");
-    if (!swiperContainer) return; // Проверяем наличие контейнера для карусели
+const swiperCarousel = () => {
+  const swiper = new Swiper(".swiper", {
+    modules: [Navigation, Pagination],
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true,
 
-    const swiper = new Swiper(swiperContainer, {
-      modules: [Navigation, Pagination],
-      slidesPerView: 1,
-      spaceBetween: 10,
-      pagination: {
-        el: ".swiper-pagination",
-        type: "progressbar",
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+        spaceBetween: 20,
       },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      700: {
+        slidesPerView: 3,
+        spaceBetween: 20,
       },
-      loop: true,
-      breakpoints: {
-        500: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        700: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-        },
-        900: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-        },
+      900: {
+        slidesPerView: 4,
+        spaceBetween: 40,
       },
-    });
-  };
-
-  // Отложенная инициализация карусели
-  document.addEventListener("DOMContentLoaded", initializeSwiper);
+    },
+  });
 };
 
-export default initSwiperCarousel;
+export default swiperCarousel;
