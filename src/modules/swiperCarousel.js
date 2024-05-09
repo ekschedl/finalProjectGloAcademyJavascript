89296fed5d2fa7d7e8bc6dddf362dc161f6e10e6
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-05-10 01:38:33 +0200

    +css for swiper

--- src/modules/swiperCarousel.js
+++ src/modules/swiperCarousel.js
@@ -1,37 +1,43 @@
 import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
-// import Swiper and modules styles
-const swiperCarousel = () => {
-  console.log("gee");
-  const swiper = new Swiper(".swiper", {
-    modules: [Navigation, Pagination],
-    slidesPerView: 1,
-    spaceBetween: 10,
-    pagination: {
-      el: ".swiper-pagination",
-      type: "progressbar",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    loop: true,
 
-    breakpoints: {
-      500: {
-        slidesPerView: 2,
-        spaceBetween: 20,
+const initSwiperCarousel = () => {
+  const initializeSwiper = () => {
+    const swiperContainer = document.querySelector(".swiper");
+    if (!swiperContainer) return; // Проверяем наличие контейнера для карусели
+
+    const swiper = new Swiper(swiperContainer, {
+      modules: [Navigation, Pagination],
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
       },
-      700: {
-        slidesPerView: 3,
-        spaceBetween: 20,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
-      900: {
-        slidesPerView: 4,
-        spaceBetween: 40,
+      loop: true,
+      breakpoints: {
+        500: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        700: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        900: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
       },
-    },
-  });
+    });
+  };
+
+  // Отложенная инициализация карусели
+  document.addEventListener("DOMContentLoaded", initializeSwiper);
 };
 
-export default swiperCarousel;
+export default initSwiperCarousel;
